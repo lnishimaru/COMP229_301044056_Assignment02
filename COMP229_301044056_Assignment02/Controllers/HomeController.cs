c7b80c6dae4ad9c5219a4d682d61c2f9ba846361
Lilian Nishimaru De Souza <lilian.nishimaru@gmail.com>
2019-10-10 23:08:44 -0400

    Add project files.

---
+++ COMP229_301044056_Assignment02/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using COMP229_301044056_Assignment02.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace COMP229_301044056_Assignment02.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            System.Diagnostics.Debug.WriteLine("carregando a view principal");
+            foreach (Recipe r in FakeRepository.Recipes)
+            {
+                System.Diagnostics.Debug.WriteLine(r.Name);
+            }
+            //return View(FakeRepository.Recipes); 
+            return View();
+        }
+       [HttpGet]
+        public ViewResult InsertPage(Recipe recipe)
+        {
+            recipe.ID = 1001;
+            recipe.Category = "Test";
+            recipe.Ingredients = "Heavy cream, Unflavored gelatin, Milk , White Sugar, Vanilla extract";
+            System.Diagnostics.Debug.WriteLine("Second Step");
+            return View();
+        }
+        [HttpPost]
+        public ViewResult AddRecipe(Recipe recipe)
+        {
+            System.Diagnostics.Debug.WriteLine("1.Saving  recipe?");
+            System.Diagnostics.Debug.WriteLine(recipe.Name);
+            FakeRepository.AddItem(recipe);
+            foreach(Recipe r in FakeRepository.Recipes)
+            {
+                System.Diagnostics.Debug.WriteLine(r.Name);
+            }
+            return View("Index");
+        } 
+    }
+
+}
---
+++ COMP229_301044056_Assignment02/Models/Recipe.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace COMP229_301044056_Assignment02.Models
+{
+    public class Recipe
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Cuisine { get; set; }
+        public string Ingredients { get; set; }
+        public string Instructions { get; set; }
+    }
+}
