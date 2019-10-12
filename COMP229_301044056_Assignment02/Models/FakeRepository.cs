385136c42e5246e1d5688cc5a7af13efa7081786
Lilian Nishimaru De Souza <lilian.nishimaru@gmail.com>
2019-10-12 14:58:23 -0400

    Final Adjustments

--- COMP229_301044056_Assignment02/Models/FakeRepository.cs
+++ COMP229_301044056_Assignment02/Models/FakeRepository.cs
@@ -9,10 +9,10 @@ namespace COMP229_301044056_Assignment02.Models
     {
         //private static List<Recipe> recipes = new List<Recipe>();
 
-        private static List<Recipe> recipes = new List<Recipe>() { 
+        private static List<Recipe> recipes = new List<Recipe>() {
                 new Recipe { ID=301,Name = "Panna Cotta", Category="Desserts",Cuisine="Italian",Ingredients="Heavy cream, Unflavored gelatin, Milk , White Sugar, Vanilla extract",Instructions="Mix all ingredients in a pan and stir until boil in medium heat." },
                 new Recipe { ID=101,Name = "Autumn Soup", Category="Appetizers",Cuisine="American",Ingredients="Butternut squash, Vegetable broth, Coconut oil, Curry powder, Honewycrisp apple, Onion, Almond Milk, Salt, Cinnamon",Instructions="Cook the squash with onion apple and almond milk. When softens, put to the food processor." },
-                new Recipe { ID=201,Name = "Roasted Beef", Category="Entrees",Cuisine="Mexican",Ingredients="Beef, Avocado, Tomatoes, Salt, Pepper, Cummin, Cilantro, Onion, Lemon, Tortillas",Instructions="Grill the beef seasoned with salt, pepper and cummin. Season the dice the tomatoes, onions and avocado with lemon, salt, pepper and cilantro. Put the toppings in the tortillas"} }; 
+                new Recipe { ID=201,Name = "Beef Taco", Category="Entrees",Cuisine="Mexican",Ingredients="Beef, Avocado, Tomatoes, Salt, Pepper, Cummin, Cilantro, Onion, Lemon, Tortillas",Instructions="Grill the beef seasoned with salt, pepper and cummin. Season the dice the tomatoes, onions and avocado with lemon, salt, pepper and cilantro. Put the toppings in the tortillas"} };
         public static void AddItem(Recipe recipe)
         {
             recipes.Add(recipe);
@@ -23,8 +23,23 @@ namespace COMP229_301044056_Assignment02.Models
             {
                 return recipes;
             }
-     
+
         }
-       
+
+        private static Dictionary<int, string> photos = new Dictionary<int, string>()
+        {
+            {301,"~/PannaCotta.jpg" },
+            {101, "~/AutumnSoup.jpg" },
+            {201, "~/BeefTaco.jpg" },
+            {999,"~/Logo.jpg" }
+        };
+        public static IDictionary<int, string> Photos
+        {
+            get
+            {
+                return photos;
+            }
+
+        }
     }
 }
